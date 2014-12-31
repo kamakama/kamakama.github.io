49c4285e363242330bfa2bf413194c648fa7e552
kamakama <kama-chan@t-online.de>
2014-12-31 23:11:08 +0100

    recs div neues design

--- recs/js/script.js
+++ recs/js/script.js
@@ -1,8 +1,37 @@
 $(document).ready(function() {
-    $("#imgselect img").hover(function() {
+
+	//hover effects
+	$(".rec h3").hover(function() {
+		$(this).css("background-color", "#646972");
+	},function() {
+		$(this).css("background-color", "#4b4a4a");
+	});
+	
+	$("#imgselect img").hover(function() {
 		$(this).css("opacity", "0.7");
-	},function(){
+	},function() {
 		$(this).css("opacity", "1");
+	});
+	
+	//collapse or expand recs
+	$(".rec h3").click(function() {
+		$("p", $(this).parent(".rec")).slideToggle( 200, "linear" );
+	});
+	
+	var hiddenstate = true;
+	$("#collapse").click(function() {
+		if (hiddenstate == true) {
+			$("#recs p").css("display", "block");
+			hiddenstate=false;
+		} else {
+			$("#recs p").css("display", "none");
+			hiddenstate=true;
+		}
+	});
+	
+	// change displayed mascot on click
+	$(".mascot").click(function(){
+		$("body").css("background-image", "url('img/mascot/"+$(this).attr("alt")+".png')");
 	});
 	
 	$("#kirinolink").click(function(){
@@ -13,8 +42,4 @@ $(document).ready(function() {
 		$("body").css("background-image", "url('img/mascot/ayase.png')");
 	});
 	
-	// change displayed mascot on click
-	$(".mascot").click(function(){
-		$("body").css("background-image", "url('img/mascot/"+$(this).attr("alt")+".png')");
-	});
 });
