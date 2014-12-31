f6bd896af5191195feda9f080c3990ced6f0b4d2
kamakama <kama-chan@t-online.de>
2014-12-31 18:00:58 +0100

    added mascots, rewrite js
    changed asuka, akari, tsurugi, kirino, shinobu
    +alice
    +miuna
    +poplar
    +korbo
    +maki
    +ryuuko
    +ayase
    +cosplay-chan
    -nils
    -lain

--- recs/js/script.js
+++ recs/js/script.js
@@ -3,21 +3,18 @@ $(document).ready(function() {
 		$(this).css("opacity", "0.7");
 	},function(){
 		$(this).css("opacity", "1");
-	})
+	});
+	
+	$("#kirinolink").click(function(){
+		$("body").css("background-image", "url('img/mascot/kirino.png')");
+	});
+	
+	$("#ayaselink").click(function(){
+		$("body").css("background-image", "url('img/mascot/ayase.png')");
+	});
+	
+	// change displayed mascot on click
+	$(".mascot").click(function(){
+		$("body").css("background-image", "url('img/mascot/"+$(this).attr("alt")+".png')");
+	});
 });
-
-// change displayed mascot
-function mascot(waifu) {
-	switch (waifu) {
-		case 0: waifuimg = "url('')"; break;
-		case 1: waifuimg = "url('img/kirino.png')"; break;
-		case 2: waifuimg = "url('img/akari.png')"; break;
-		case 3: waifuimg = "url('img/tsurugi.png')"; break;
-		case 4: waifuimg = "url('img/nils.png')"; break;
-		case 5: waifuimg = "url('img/asuka.png')"; break;
-		case 6: waifuimg = "url('img/mayaka.png')"; break;
-		case 7: waifuimg = "url('img/shinobu.png')"; break;
-		case 8: waifuimg = "url('img/lain.png')"; break;
-	}
-	$("body").css("background-image", waifuimg);
-}
